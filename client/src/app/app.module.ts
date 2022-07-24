a1d3bfb50d3d231fff2dae65cc51ba5b84ca0dfa
dukanacv <dukanacvla@gmail.com>
2022-07-24 21:06:15 +0200

    added login, register form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,12 +7,18 @@ import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { TabsModule } from 'ngx-bootstrap/tabs';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    FooterComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
